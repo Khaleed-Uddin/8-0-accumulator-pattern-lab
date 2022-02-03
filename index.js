0b02ee1bbe82904c14e38220ade7345b85ffe3f8
Khaleed Uddin <uddin1786@gmail.com>
2022-02-02 22:11:38 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,78 @@
 /**
  * Returns a boolean representing whether or not all values are numbers.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
- * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
+ * @returns {boolean} Returns `true` if all values in the array are numbers. 
+ * Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+let number = true;
+for(let i = 0; i < rolls.length; i++){
+if(typeof rolls[i] !== 'number'){
+  number = false;
+  break;
+} 
+}
+return number;
+}
 
 /**
- * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
+ * Finds a value in an array. If that value is in the array, returns it. 
+ * Otherwise, returns `null`.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+let found = null;
+for(let i = 0; i < rolls.length; i++){
+  if(rolls[i] === value){
+    found = value;
+    break;
+  }
+}
+return found;
+}
 
 /**
- * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
+ * Returns a new array from the `rolls` array with only values equal to or greater 
+ * than the `lowest` value.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
- * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
+ * @returns {number[]} An array of all numbers that are equal to or higher than 
+ * the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+let array = [];
+for(let i = 0; i < rolls.length; i++){
+if(rolls[i] >= lowest){
+  array.push(rolls[i]);
+}
+}
+return array;
+}
+
+
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
- * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
+ * @returns {object} An object where the keys are numbers rolled and the values 
+ * are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+let object = {};
+for(let i = 0; i < rolls.length; i++){
+if(object[rolls[i]]){
+  object[rolls[i]] += 1;
+} else {
+  object[rolls[i]] = 1;
+}
+}
+return object;
+}
+
+
+
 
 // Do not change the code below here.
 module.exports = {
